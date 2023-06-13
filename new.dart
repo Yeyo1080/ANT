bbe30a7952424372cde68161032077ecd0f60c2e
Yeyo1080 <120669807+Yeyo1080@users.noreply.github.com>
2023-06-12 22:43:08 -0600

    Add files via upload

---
+++ new.dart
@@ -0,0 +1,14 @@
+import 'dart:html';
+
+void main() {  
+    querySelector('#concat')?.onClick.listen(funcion);
+}
+  
+void funcion(MouseEvent e)
+{
+    String? s1 = (querySelector('#name') as InputElement).value;
+    String? s2 = (querySelector('#num') as InputElement).value;
+    
+
+    querySelector('#output')?.text = 'farmacia registrada: $s1';
+}
